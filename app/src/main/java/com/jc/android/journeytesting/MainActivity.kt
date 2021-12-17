35e2bab5213d007e9250310df29afca93747cfd1
Juechen Wang <wjc.zhw@gmail.com>
2021-12-17 20:50:41 +1300

    Initial commit - create project.

---
+++ app/src/main/java/com/jc/android/journeytesting/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jc.android.journeytesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jc.android.journeytesting.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/jc/android/journeytesting/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.jc.android.journeytesting.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
